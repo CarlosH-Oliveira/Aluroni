b074d056b4b86ae78c52c04a39e90c62131b8cc3
unknown <carlosholiveiradev@outlook.com>
2024-01-04 08:36:47 -0300

    correção do botão de voltar

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -37,7 +37,7 @@ const NavBar: FunctionComponent = () => {
 						</div>
 					))}
 				</div>
-				<div onClick={() => navigate(-1)} className="navBar-return">Voltar</div>
+				<button onClick={() => navigate(-1)} className="navBar-return">Voltar</button>
 			</nav>
 		</>
 	);
